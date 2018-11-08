dd358a03c6faefefdd2aec054b11123c30763e50
Google Cloud <google.cloud@itatix.com>
2018-11-07 23:22:15 -0600

    improved webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ module.exports = (env) => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
-    },
+    }, 
     module: {
       rules: [{
         loader: 'babel-loader',
@@ -42,7 +42,8 @@ module.exports = (env) => {
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
   };
 };
